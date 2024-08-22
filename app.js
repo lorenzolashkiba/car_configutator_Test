833e67af32435fbe81d6b897464605d19032bfaf
lorenzolashkiba <lorenzoboiko@gmail.com>
2024-08-22 14:05:51 +0200

    for vercel

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// server.js
 const express = require('express');
-const path = require('path')
+const path = require('path');
 const app = express();
-const port = 3000;
-app.use(express.static('public'))
-app.set('view engine', 'pug');
-app.get('/', (req, res) => {
-    res.render('index');
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index'); // Make sure you have a `views/index.pug` file
 });
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
